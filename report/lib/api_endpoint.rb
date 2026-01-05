4b2df15d464a649246e90e20603f7fed9dcd935e
Fernando Briano <fernando@picandocodigo.net>
2026-01-05 14:46:12 +0000

    Updates report to show Serverless APIs only on main
    The release branch reports should only report on the Stack version
    tests. The tests are still there, this just removes reporting on them
    on 9.x branches: Removes the Serverless columns in the endpoints table
    and the summary of API and coverage.
    
    The code uses the value in ENV for BRANCH, which is either `main` or
    `major.minor` for release branches. Since Serverless doesn't have a
    version but the API updates, I think it makes sense to report on it
    only in main so we don't get PRs in the automation for every release
    branch when Serverless updates.

--- report/lib/api_endpoint.rb
+++ report/lib/api_endpoint.rb
@@ -47,6 +47,8 @@ module Elastic
   end
 
     def available_serverless?
+      return true if serverless_only?
+
       @available_serverless ||= @availability.nil? || (
         @availability['serverless'] &&
         @availability.dig('serverless', 'visibility') == 'public'
